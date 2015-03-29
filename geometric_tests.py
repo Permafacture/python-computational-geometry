c85b729b48f10ed72cc213c341bcd1949c75bf2f
Elliot <elbiot@elbitop>
2015-03-29 18:34:02 -0500

    Don't know if code is working, about to wash it all away

--- geometric_tests.py
+++ geometric_tests.py
@@ -37,8 +37,8 @@ def segments2d_intersection_segments2d(segs1,segs2):
     between arrays of line segments segs1 (n,2) and segs2 (m,2),
     and an (n,m,1) mask to distinguish segments that did not
     intersect.'''
-    r1s = segs1.data[np.newaxis,:]
-    r2s = segs2.data[:,np.newaxis]
+    r1s = segs1._points[np.newaxis,:]
+    r2s = segs2._points[:,np.newaxis]
     x1,y1,x2,y2 = r1s[:,:,0],r1s[:,:,1],r1s[:,:,2],r1s[:,:,3]
     x3,y3,x4,y4 = r2s[:,:,0],r2s[:,:,1],r2s[:,:,2],r2s[:,:,3]
     
@@ -49,11 +49,83 @@ def segments2d_intersection_segments2d(segs1,segs2):
     ub = numb/den
     xs = x1 + ua*(x2-x1)
     ys = y1 + ua*(y2-y1)
-    #mask = (den != 0) & (numa != 0)
+    mask = (den != 0) & (numa != 0)
     mask = (ua>=0) & (ua<=1) & (ub>=0) & (ub<=1)
     xs[mask] = np.inf
     return np.array([xs,ys]).T,mask
 
+@reversible_args('segments3d','triangles')
+def segments3d_intersection_triangles(segs,tri):
+    '''http://geomalgorithms.com/a06-_intersect-2.html#intersect3D_RayTriangle%28%29'''
+
+    '''
+    #define SMALL_NUM   0.00000001 // anything that avoids division overflow
+    // dot product (3D) which allows vector operations in arguments
+    #define dot(u,v)   ((u).x * (v).x + (u).y * (v).y + (u).z * (v).z)
+     
+
+
+    // intersect3D_RayTriangle(): find the 3D intersection of a ray with a triangle
+    //    Input:  a ray R, and a triangle T
+    //    Output: *I = intersection point (when it exists)
+    //    Return: -1 = triangle is degenerate (a segment or point)
+    //             0 =  disjoint (no intersect)
+    //             1 =  intersect in unique point I1
+    //             2 =  are in the same plane
+    int
+    intersect3D_RayTriangle( Ray R, Triangle T, Point* I )
+    {
+        Vector    u, v, n;              // triangle vectors
+        Vector    dir, w0, w;           // ray vectors
+        float     r, a, b;              // params to calc ray-plane intersect
+
+        // get triangle edge vectors and plane normal
+        u = T.V1 - T.V0;
+        v = T.V2 - T.V0;
+        n = u * v;              // cross product
+        if (n == (Vector)0)             // triangle is degenerate
+            return -1;                  // do not deal with this case
+
+        dir = R.P1 - R.P0;              // ray direction vector
+        w0 = R.P0 - T.V0;
+        a = -dot(n,w0);
+        b = dot(n,dir);
+        if (fabs(b) < SMALL_NUM) {     // ray is  parallel to triangle plane
+            if (a == 0)                 // ray lies in triangle plane
+                return 2;
+            else return 0;              // ray disjoint from plane
+        }
+
+        // get intersect point of ray with triangle plane
+        r = a / b;
+        if (r < 0.0)                    // ray goes away from triangle
+            return 0;                   // => no intersect
+        // for a segment, also test if (r > 1.0) => no intersect
+
+        *I = R.P0 + r * dir;            // intersect point of ray and plane
+
+        // is I inside T?
+        float    uu, uv, vv, wu, wv, D;
+        uu = dot(u,u);
+        uv = dot(u,v);
+        vv = dot(v,v);
+        w = *I - T.V0;
+        wu = dot(w,u);
+        wv = dot(w,v);
+        D = uv * uv - uu * vv;
+
+        // get and test parametric coords
+        float s, t;
+        s = (uv * wv - vv * wu) / D;
+        if (s < 0.0 || s > 1.0)         // I is outside T
+            return 0;
+        t = (uv * wu - uu * wv) / D;
+        if (t < 0.0 || (s + t) > 1.0)  // I is outside T
+            return 0;
+
+        return 1;                       // I is in T
+    }
+     '''
 @reversible_args('segments2d','aabbs')
 def segments2d_intersects_aabb(segs,aabb):
     print "got called!",segs._type,aabb
--- geometries.py
+++ geometries.py
@@ -5,6 +5,11 @@ import geometric_tests
 from types import MethodType
 import numpy as np
 
+## TODO make numpy array read only (geometry._data)
+## TODO make segment, ray and line all the same
+##       just let them apply their own parameter 
+##       constraint in a method.
+
 def test_key(key):
     '''function for filtering geometric_tests.__dict__ keys
     based on whats a geometric test and what isn't'''
@@ -68,7 +73,7 @@ def key2method2(geomtype):
       def dispatcher(*args,**kwargs):
         '''takes a dictionary of {name: func} and calls the
         correct function for name.'''
-        print args
+        #print args
         self=args[0]
         other=args[1]
         try: return fdict[other._type](self,other)
@@ -94,7 +99,7 @@ def geometry(cls):
         for key,func in key2method2(cls._type).items():
           #setattr(Segments2d,key,MethodType(func,Segments2d))
           setattr(cls,key,MethodType(func,None,cls))
-          print "set attr"
+          #print "set attr"
         return cls(*args,**kwargs)
     give_attributes.__name__ = cls.__name__
     give_attributes.__doc__ = cls.__doc__
@@ -103,55 +108,36 @@ def geometry(cls):
 
 class Geometry(object):
     '''Meta class for all geometry objects'''
-    def __init__(self,shape=None,dtype=np.float64):
-        '''shape is the shape of an array of one geometry of
-        this type.'''
-        self._data   = None #user accessible
-        self.to_add = []
-        self.shape=shape
-        self.dtype=dtype
-        self.properties = []
+    def __init__(self,_geoms,shape=None,dtype=None):
+        '''geoms is a list or numpy array of the geometry data'''
+        if type(_geoms) == np.ndarray :
+          if type(_geoms[0]) == np.ndarray:
+            geoms=_geoms
+            if geoms.dtype != dtype:
+              geoms=np.array(geoms,dtype=dtype)
+          else:
+            geoms = np.array([_geoms],dtype=dtype)
 
-    def add_data(self,geoms):
-        '''add geometries to this object, either as list or 
-        numpy array. either one at a time or a list/array
-        of many.  self.data is set to None, and will be 
-        re-compiled when next accessed'''
-        geoms = np.array(geoms,dtype=self.dtype)
-        if geoms.shape == self.shape:
-          #this is just one geometry
-          geoms=np.array([geoms])
-        elif geoms.shape[1:] == self.shape:
-          pass
+
         else:
-          raise ValueError("Expected shape: Nx%s | got: %s"%(self.shape,geoms.shape))
-        self.to_add.append(geoms)
+            geoms = np.array(_geoms,dtype=dtype)
+            if geoms.dtype==object:
+              e = "Geometry can't instantiate with a %s"%type(_geom)
+              raise ValueError(e)
+            elif type(geoms[0]) != np.ndarray:
+              geoms = np.array([geoms],dtype=dtype)
+        if geoms[0].shape == shape:
+          self._data = geoms
+          self._properties = []
+        else:
+          e = "expected geometry.shape == %s, got %s"%(shape,geoms.shape)
+          raise ValueError(e)
 
-    def clear_properties(self):
+    def _clear_cache(self):
         '''sets all computed properties to None, for when the
         properties are dirty and need to be recalculated if used'''
-        for p in self.properties:
-          setattr(self,'_%s'%p,None)
-   
-    def compile(self):
-        '''Append recently added data to older data.
-        after this, self.data will have all geometries
-        and can be accessed without further computations''' 
-
-        if self._data:
-          geoms = [self._data]+to_add
-        else:
-          geoms = self.to_add
-        self._data = np.concatenate(geoms)
-        self.to_add = []
-        self.clear_properties()
-
-    @property
-    def data(self):
-      if self.to_add:
-        self.compile()
-      return self._data
-       
+        for p in self._properties:
+          setattr(self,'_%s'%p,None)   
 
 @geometry
 class Segments2d(Geometry):
@@ -159,14 +145,16 @@ class Segments2d(Geometry):
     [x_begin,y_begin,x_end,y_end]'''
     _type = 'segments2d'
 
-    def __init__(self):
-        Geometry.__init__(self,shape=(4,))
+    def __init__(self,geoms,dtype=None):
+        Geometry.__init__(self,geoms,shape=(4,),dtype=dtype)
+        #rename _data to make sense for a line segment
+        self._points = self._data 
 
         #boiler plate: append to properties, clear_properties
         #define @property decorated property.  write a 
         #create_property(self)
-        self.properties.append('normals')
-        self.clear_properties()
+        self._properties.append('normals')
+        self._clear_cache()
 
     @property
     def normals(self):
@@ -176,7 +164,7 @@ class Segments2d(Geometry):
 
     def create_normals(self):
         '''segs = [[x1,y1,x2,y2],...] => normals [[xn,yn],...]'''
-        segs = self.data
+        segs = self._points
         result = segs[:,2:] - segs[:,:2]
         tmp = np.empty((len(segs),3),dtype=segs.dtype)
         tmp[:,:2] = np.square(result)
@@ -187,23 +175,105 @@ class Segments2d(Geometry):
         result[:,1] = result[:,1] / tmp[:,2]
         self._normals = result
       
+@geometry
+class Segments3d(Geometry):
+    '''An array of 3D line segments defined by
+    [x_begin,y_begin,z_begin,x_end,y_end,z_end]'''
+    _type = 'segments3d'
+
+    def __init__(self,geoms,dtype=None):
+        Geometry.__init__(self,geoms,shape=(6,),dtype=dtype)
+        #rename _data to make sense for a line segment
+        self._points = self._data 
+
+        #boiler plate: append to properties, clear_properties
+        #define @property decorated property.  write a 
+        #create_property(self)
+        self._properties.append('normals')
+        self._clear_cache()
+
+    @property
+    def normals(self):
+      if not self._normals:
+        self.create_normals()
+      return self._normals
+
+    def create_normals(self):
+        '''segs = [[x1,y1,z1,x2,y2,z2],...] => normals [[xn,yn,zn],...]'''
+        segs = self._points
+        result = segs[:,3:] - segs[:,:3]
+        tmp = np.empty((len(segs),4),dtype=segs.dtype)
+        tmp[:,:3] = np.square(result)
+        #noticed that if the result of sum goes to an element
+        #of the same array, that element is not part of the sum.
+        #tmp[:,3] here is still garbage, so we can write over it
+        tmp[:,3] = np.sum(tmp,axis=1,out=tmp[:,3]) # SAFE? seems to be
+        tmp[:,3] = np.sqrt(tmp[:,3])
+        #lame, why do this in two step?
+        result[:,0] = result[:,0] / tmp[:,3]  #TODO use newaxis 
+        result[:,1] = result[:,1] / tmp[:,3]  # and do 3 in 1!
+        result[:,2] = result[:,2] / tmp[:,3]
+        self._normals = result
+
+@geometry
+class Triangles(Geometry):
+    '''An array of 3D triangles defined by
+    [[v0,v1,v2],...] where v0 = [x0,y0,z0]'''
+    _type = 'triangles'
+
+    def __init__(self,geoms,dtype=None):
+        Geometry.__init__(self,geoms,shape=(3,3),dtype=dtype)
+        #rename _data to make sense for a line segment
+        self._points = self._data 
+
+        #boiler plate: append to properties, clear_properties
+        #define @property decorated property.  write a 
+        #create_property(self)
+        self._properties.extend(['normals','parameters'])
+        self._clear_cache()
+
+
+    @property
+    def parameters(self):
+      if not self._parameters:
+        self.create_parameters()
+      return self._parameters
+
+    def create_parameters(self):
+        '''Parametric triangle:
+        for s + t <= 1
+        V(s,t) = v0 + s*(v1-v0) + t*(v2-v0)
+        or V(s,t) = v0 + s*u + t*v
+        result: [v0,v1,v2] ==> [v0,u,v]'''
+        points = self._points.copy()
+
+        #define a new view on the array, all v0's together
+        v0s,v1s,v2s = (points[:,i,:] for i in range(3))
+        np.subtract(v1,v0,out=v1)
+        np.subtract(v2,v0,out=v2)
+        self._parameters = points
+        self._normals = np.cross(v1s,v2s)
+        
+    @property
+    def normals(self):
+      if not self._normals:
+        self.create_parameters()
+      return self._normals
 
 if __name__ == '__main__':
   n = 10
   t = np.linspace(0,2*np.pi,n)
 
-  print "create segments"
-  segs1 = Segments2d()
+  print "Test: create segments"
   rays1 = np.zeros((n,4),dtype=np.float32)
   rays1[:,2] = np.cos(t)
   rays1[:,3] = np.sin(t)
-  segs1.add_data(rays1)
+  segs1 = Segments2d(rays1)
 
-  segs2 = Segments2d()
   rays2 =np.ones((n,4),dtype=np.float32)
   rays2[:,2] = 1+np.cos(t)
   rays2[:,3] = np.sin(t)
-  segs2.add_data(rays2)
+  segs2 = Segments2d(rays2)
 
   pts1,mask1 = segs1.intersection(segs2)
   pts2,mask2 = segs2.intersection(segs1)
